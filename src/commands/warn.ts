e38b6880467b23ed86928393f7eed6d7741ca935
Petr Kostenko <picalines0@gmail.com>
2020-01-04 22:46:42 +0300

    setMaxWarns

--- src/commands/warn.ts
+++ src/commands/warn.ts
@@ -1,7 +1,7 @@
 import { Command, CommandInfo, ArgumentEnumerator, findCommand } from "../command";
 import { getMemberFromMention } from "../utils";
-import { getGuildData } from "../guildData";
 import { MemberIsBotError } from "../error";
+import { getGuildData } from "../guildData";
 import { Message } from "discord.js";
 import { BanCommand } from "./ban";
 
@@ -15,19 +15,17 @@ export class WarnCommand extends Command {
     };
 
     async run(msg: Message, argEnumerator: ArgumentEnumerator) {
-        let memberMention = this.readNextToken(argEnumerator, "user", "ожидалось упоминание участника сервера");
+        const memberMention = this.readNextToken(argEnumerator, "user", "ожидалось упоминание участника сервера");
 
-        let member = getMemberFromMention(msg.guild, memberMention);
-        if (member.user.bot) {
-            throw new MemberIsBotError(memberMention);
-        } else if (member.permissions.has("ADMINISTRATOR")) {
+        const member = getMemberFromMention(msg.guild, memberMention);
+        if (member.permissions.has("ADMINISTRATOR")) {
             throw new Error("нельзя кинуть предупреждение администратору");
         }
 
-        let guildData = getGuildData(msg);
+        const guildData = getGuildData(msg);
 
-        let maxWarns = guildData.getProperty("maxWarns", 3).value;
-        let memberWarns = guildData.getAccount(member).getProperty("warns", 0);
+        const maxWarns = guildData.getProperty("maxWarns", 3).value;
+        const memberWarns = guildData.getAccount(member).getProperty("warns", 0);
         memberWarns.value += 1;
 
         await msg.channel.send(`${msg.member} кинул предупреждение ${member} (${memberWarns.value}/${maxWarns})`);
@@ -53,9 +51,9 @@ export class UnwarnCommand extends Command {
     };
 
     async run(msg: Message, argEnumerator: ArgumentEnumerator) {
-        let memberMention = this.readNextToken(argEnumerator, "user", "ожидалось упоминание извинившегося участника сервера");
+        const memberMention = this.readNextToken(argEnumerator, "user", "ожидалось упоминание извинившегося участника сервера");
 
-        let member = getMemberFromMention(msg.guild, memberMention);
+        const member = getMemberFromMention(msg.guild, memberMention);
         if (member.user.bot) {
             throw new MemberIsBotError(memberMention);
         }
@@ -63,13 +61,13 @@ export class UnwarnCommand extends Command {
             throw new Error("нельзя снять предупреждение с самого себя");
         }
 
-        let guildData = getGuildData(msg);
-        let acc = guildData.getAccount(member);
-        let warnsProperty = acc.getProperty<number>("warns");
+        const guildData = getGuildData(msg);
+        const acc = guildData.getAccount(member);
+        const warnsProperty = acc.getProperty<number>("warns");
 
         if (warnsProperty && warnsProperty.value > 0) {
             warnsProperty.value -= 1;
-            let maxWarns = guildData.getProperty("maxWarns", 3).value;
+            const maxWarns = guildData.getProperty("maxWarns", 3).value;
             await msg.channel.send(`${msg.member} снял предупреждение с ${member} (${warnsProperty.value}/${maxWarns})`);
         }
         else {
@@ -79,5 +77,24 @@ export class UnwarnCommand extends Command {
         if (warnsProperty && warnsProperty.value <= 0) {
             acc.removeProperty("warns");
         }
+    }
+}
+
+export class MaxWarnsCommand extends Command {
+    info: CommandInfo = {
+        name: "setMaxWarns",
+        syntax: [["int", "n"]],
+        description: "ставит максимальное число предупреждений до бана",
+        permission: "admin",
+        group: "Администрирование"
+    };
+
+    async run(msg: Message, argEnumerator: ArgumentEnumerator) {
+        const n = Number(this.readNextToken(argEnumerator, "int", "ожидалось число предупреждений"));
+        if (n <= 0) {
+            throw new Error("Это аморально");
+        }
+
+        getGuildData(msg).setProperty("maxWarns", n);
     }
 }
